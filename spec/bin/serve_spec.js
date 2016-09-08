dc55bfa09c55e730483f92564f166ae93965da5b
Vinson Chuong <vinsonchuong@gmail.com>
2016-09-08 07:34:32 -0700

    Serve a simple index.html

--- spec/bin/serve_spec.js
+++ spec/bin/serve_spec.js
@@ -1,7 +1,60 @@
 import register from 'test-inject';
 import Directory from 'directory-helpers';
+import fetch from 'node-fetch';
+
+async function writePlugin(name) {
+  await this.write({
+    [`node_modules/${name}/package.json`]: {
+      name
+    },
+    [`node_modules/${name}/index.js`]: `
+      exports.default = function() {
+      };
+    `
+  });
+}
+
+async function start() {
+  this.server = this.spawn('npm', ['start']);
+  await this.server.filter((output) => output.match(/Listening/));
+}
+
+const inject = register({
+  project: {
+    setUp: () => new Directory('project'),
+    tearDown: async (project) => {
+      if ('server' in project) {
+        project.server.process.kill();
+      }
+      await project.remove();
+    }
+  }
+});
 
 describe('serve-bin', () => {
-  it('serves src/index.html', async () => {
-  });
+  it('serves src/index.html', inject(async ({project}) => {
+    await project.write({
+      'package.json': {
+        name: 'project',
+        private: true,
+        scripts: {
+          start: 'serve'
+        }
+      },
+      'src/index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    });
+    await project.symlink('../node_modules', 'node_modules');
+
+    await project::start();
+
+    const response = await fetch('http://localhost:8080');
+    expect(await response.text()).toBe([
+      '<!doctype html>',
+      '<meta charset="utf-8">',
+      ''
+    ].join('\n'));
+  }));
 });
--- src/bin/serve.js
+++ src/bin/serve.js
@@ -1,1 +1,16 @@
-process.stdout.write('Hello World!\n');
+import {Server} from 'http';
+import Directory from 'directory-helpers';
+
+const server = new Server();
+server.on('request', async (request, response) => {
+  const html = await new Directory('src').read('index.html');
+  response.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+    'Content-Length': Buffer.byteLength(html)
+  });
+  response.write(html);
+  response.end();
+});
+server.listen(8080, () => {
+  process.stdout.write('Listening on :8080');
+});
