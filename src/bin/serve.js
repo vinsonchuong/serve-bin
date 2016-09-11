73b00596467586837b5b46ca27a30853ac783f64
Vinson Chuong <vinsonchuong@gmail.com>
2016-09-10 20:22:36 -0700

    Use send to serve static assets

--- src/bin/serve.js
+++ src/bin/serve.js
@@ -1,18 +1,13 @@
 import {Server} from 'http';
+import * as url from 'url';
 import Directory from 'directory-helpers';
+import send from 'send';
 
 const root = new Directory('src');
 const server = new Server();
-server.on('request', async (request, response) => {
-  const path = request.url.slice(1) || 'index.html';
-  const file = await root.read(path);
-
-  response.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8',
-    'Content-Length': Buffer.byteLength(file)
-  });
-  response.write(file);
-  response.end();
+server.on('request', (request, response) => {
+  send(request, url.parse(request.url).pathname, {root: root.path()})
+    .pipe(response);
 });
 server.listen(8080, () => {
   process.stdout.write('Listening on :8080\n');
